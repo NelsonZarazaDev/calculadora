1d9b857801f39d8f4634eb01ed0e227d4059b621
NelsonZaraza <nelsonmauricionavarrozaraza@gmail.com>
2023-12-29 21:42:44 -0500

    Calculadora terminada usando la libreria mathjs con React

---
+++ src/component/Pantalla.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../css/Pantalla.css"
+
+//Se usa const para componentes sencilloos
+const Pantalla = ({input}) => (
+    <div className="input">
+        {input}
+    </div>
+);
+
+export default Pantalla;
